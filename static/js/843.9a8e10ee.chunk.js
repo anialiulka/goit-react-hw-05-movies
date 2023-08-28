85bce67f37cbc3a82e8a69ef1a61636fecb757f8
anialiulka <116043828+anialiulka@users.noreply.github.com>
2023-08-28 16:03:33 +0000

    Deploying to gh-pages from @ anialiulka/goit-react-hw-05-movies@6cc955ec6b63a06ec15fe6fe44aab4f00eefc532 🚀

---
+++ static/js/843.9a8e10ee.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[843],{843:function(e,t,r){r.r(t),r.d(t,{default:function(){return i}});var a=r(689),l=r(87),n="MovieGalleryItem_listItem__eMw-e",s="MovieGalleryItem_link__dhun7",c=r(184),i=function(e){var t=e.title,r=e.id,i=(0,a.TH)();return(0,c.jsxs)("li",{className:n,children:[(0,c.jsx)(l.rU,{to:"/movies/".concat(r),state:{from:i},className:s,children:t}),";"]})}}}]);
+//# sourceMappingURL=843.9a8e10ee.chunk.js.map

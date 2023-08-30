6d752a5cbfdd8b5400aee0fdb7cfdc0e8223d542
ania.liulka <anialiulka@gmail.com>
2023-08-30 10:46:43 -0400

    Removed lazy load from the unnecessary section

--- src/components/MovieGallery/MovieGallery.js
+++ src/components/MovieGallery/MovieGallery.js
@@ -1,10 +1,6 @@
-import { lazy, Suspense } from 'react';
 import css from './MovieGallery.module.css';
 import PropTypes from 'prop-types';
-
-const MovieGalleryItem = lazy(() =>
-  import('../MovieGalleryItem/MovieGalleryItem')
-);
+import MovieGalleryItem from 'components/MovieGalleryItem/MovieGalleryItem';
 
 const MovieGallery = ({ movies }) => {
   return (
@@ -12,9 +8,7 @@ const MovieGallery = ({ movies }) => {
       <h1 className={css.heading}>Trending movies</h1>
       <ul className={css.list}>
         {movies.map(movie => (
-          <Suspense key={movie.id} fallback={<div>Loading...</div>}>
-            <MovieGalleryItem title={movie.title} id={movie.id} />
-          </Suspense>
+          <MovieGalleryItem title={movie.title} id={movie.id} />
         ))}
       </ul>
     </div>
